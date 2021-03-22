fa2015e032d24e9bf211c42728d5b9005b6d1347
Mociej <maciekkocemba@gmail.com>
2021-03-22 17:58:44 +0100

    add init test

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -2,13 +2,26 @@ package edu.iis.mto.similarity;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class SimilarityFinderTest {
 
     @Test
     void test() {
-        fail("Not yet implemented");
+
     }
 
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void calculateJackardSimilarity() {
+    }
 }
