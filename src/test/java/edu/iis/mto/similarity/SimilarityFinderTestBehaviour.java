bc3d874857b08988df6be8e7ee9de6d5886aa1d7
Mociej <maciekkocemba@gmail.com>
2021-03-22 20:43:00 +0100

    delete mock in behaviour tests

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTestBehaviour.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTestBehaviour.java
@@ -1,6 +1,7 @@
 package edu.iis.mto.similarity;
 
-import edu.iis.mto.searcher.Mock;
+//import edu.iis.mto.searcher.Mock;
+import edu.iis.mto.searcher.SearchResult;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,13 +12,13 @@ class SimilarityFinderTestBehaviour {
     static public int[] seq1;
     static public int[] seq2;
     static public int[] seqEmpty;
-    public Mock mock;
+//    public Mock mock;
     public SimilarityFinder similarityFinder;
 
     @BeforeEach
     void init() {
-        mock = new Mock();
-        similarityFinder = new SimilarityFinder(mock);
+//        mock = new Mock();
+//        similarityFinder = new SimilarityFinder(mock);
         seq1= new int[] {1,4,56,8};
         seq2= new int[] {2,4,55,7};
         seqEmpty= new int[] {};
@@ -38,14 +39,24 @@ class SimilarityFinderTestBehaviour {
 
     @Test
     void calculateJackardSimilarityFirstNull() {
+        similarityFinder = new SimilarityFinder((elem, sequence) -> SearchResult.builder()
+                .withFound(false)
+                .build());
+
         assertThrows(NullPointerException.class, () -> similarityFinder.calculateJackardSimilarity(null, seq2));
     }
     @Test
     void calculateJackardSimilaritySecondNull() {
-        assertThrows(IllegalArgumentException.class, () -> similarityFinder.calculateJackardSimilarity(seq1, null));
+        similarityFinder = new SimilarityFinder((elem, sequence) -> SearchResult.builder()
+                .withFound(false)
+                .build());
+        assertThrows(NullPointerException.class, () -> similarityFinder.calculateJackardSimilarity(seq1, null));
     }
     @Test
     void calculateJackardSimilarityBothNull() {
+        similarityFinder = new SimilarityFinder((elem, sequence) -> SearchResult.builder()
+                .withFound(false)
+                .build());
         assertThrows(NullPointerException.class, () -> similarityFinder.calculateJackardSimilarity(null, null));
     }
 
