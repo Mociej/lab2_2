37b5727db8e8062cb7dff68722fc0a1cc4807047
Mociej <maciekkocemba@gmail.com>
2021-03-23 17:22:20 +0100

    add tests repair tests

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,25 +1,45 @@
 package edu.iis.mto.similarity;
 
 import edu.iis.mto.searcher.SearchResult;
+import edu.iis.mto.searcher.SequenceSearcher;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SimilarityFinderTest {
 
+    static public int[] seq0;
     static public int[] seq1;
     static public int[] seq2;
+    static public int[] seq3;
+    static public int[] seq4;
+    static public int[] seq5;
+    static public int[] seq6;
+    static public int[] seq7;
     static public int[] seqEmpty;
-
+    static public double res;
     public SimilarityFinder similarityFinder;
+    public SequenceSearcher sequenceSearcher;
+    public SearchResult searchResult;
+    SequenceSearcher mock;
 
     @BeforeEach
     void init() {
-        seq1= new int[] {1,4,56,8};
-        seq2= new int[] {2,4,55,7};
-        seqEmpty= new int[] {};
+        seq0 = new int[]{1, 9, 33, 42};
+        seq1 = new int[]{5, 4, 56, 8};
+        seq2 = new int[]{2, 4, 55, 7};
+        seq3 = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
+        seq4 = new int[]{2, 3, 4, 5, 6, 7};
+        seq5 = new int[]{2, 3, 4};
+        seq6 = new int[]{2, 3};
+        seq7 = new int[]{2};
+        seqEmpty = new int[]{};
     }
 
     @Test
@@ -43,6 +63,7 @@ class SimilarityFinderTest {
 
         assertThrows(NullPointerException.class, () -> similarityFinder.calculateJackardSimilarity(null, seq2));
     }
+
     @Test
     void calculateJackardSimilaritySecondNull() {
         similarityFinder = new SimilarityFinder((elem, sequence) -> SearchResult.builder()
@@ -50,6 +71,7 @@ class SimilarityFinderTest {
                 .build());
         assertThrows(NullPointerException.class, () -> similarityFinder.calculateJackardSimilarity(seq1, null));
     }
+
     @Test
     void calculateJackardSimilarityBothNull() {
         similarityFinder = new SimilarityFinder((elem, sequence) -> SearchResult.builder()
@@ -57,6 +79,8 @@ class SimilarityFinderTest {
                 .build());
         assertThrows(NullPointerException.class, () -> similarityFinder.calculateJackardSimilarity(null, null));
     }
+
+    ////////////////////////
     @Test
     void calculateJackardSimilarityFirstEmpty() {
         SimilarityFinder similarityFinder = new SimilarityFinder((elem, sequence) -> null);
@@ -76,20 +100,125 @@ class SimilarityFinderTest {
 
         assertEquals(1, similarityFinder.calculateJackardSimilarity(seqEmpty, seqEmpty));
     }
+    //////////////////////////////////
 
     @Test
     void calculateJackardSimilaritySameSeqs() {
-        SimilarityFinder similarityFinder = new SimilarityFinder((elem, sequence) -> SearchResult.builder()
-                .withFound(true)
-                .build());
-        assertEquals(1, similarityFinder.calculateJackardSimilarity(seq1, seq1));
+        sequenceSearcher = new SequenceSearcher() {
+            @Override
+            public SearchResult search(int elem, int[] sequence) {
+                for (int i = 0; i < sequence.length; i++) {
+                    if (elem == sequence[i]) {
+                        return SearchResult.builder().withFound(true).withPosition(i).build();
+
+                    }
+                }
+                return SearchResult.builder().withFound(false).withPosition(-1).build();
+            }
+        };
+
+        similarityFinder = new SimilarityFinder(sequenceSearcher);
+        assertEquals(similarityFinder.calculateJackardSimilarity(seq0, seq0), 1);
     }
 
     @Test
     void calculateJackardSimilarityNotSameSeqs() {
-        similarityFinder = new SimilarityFinder(((elem, sequence) -> SearchResult.builder().withFound(false).build()));
-        similarityFinder.calculateJackardSimilarity(seq1, seq1);
-        assertEquals(0, similarityFinder.calculateJackardSimilarity(seq1, seq2));
+
+        sequenceSearcher = new SequenceSearcher() {
+            @Override
+            public SearchResult search(int elem, int[] sequence) {
+                for (int i = 0; i < sequence.length; i++) {
+                    if (elem == sequence[i]) {
+                        return SearchResult.builder().withFound(true).withPosition(i).build();
+                    }
+                }
+                return SearchResult.builder().withFound(false).withPosition(-1).build();
+            }
+        };
+
+        similarityFinder = new SimilarityFinder(sequenceSearcher);
+        assertEquals(similarityFinder.calculateJackardSimilarity(seq0, seq1), 0.0);
+    }
+
+    //////////////////////
+    @Test
+    void calculateJackardSimilarityDiffSeqs() {
+        sequenceSearcher = new SequenceSearcher() {
+            @Override
+            public SearchResult search(int elem, int[] sequence) {
+                for (int i = 0; i < sequence.length; i++) {
+                    if (elem == sequence[i]) {
+                        return SearchResult.builder().withFound(true).withPosition(i).build();
+
+                    }
+                }
+                return SearchResult.builder().withFound(false).withPosition(-1).build();
+            }
+        };
+
+        similarityFinder = new SimilarityFinder(sequenceSearcher);
+        res = similarityFinder.calculateJackardSimilarity(seq1, seq2);
+        assertTrue(res > 0 && res < 1);
+    }
+
+    @Test
+    void calculateJackardSimilarityHalfSame() {
+        sequenceSearcher = new SequenceSearcher() {
+            @Override
+            public SearchResult search(int elem, int[] sequence) {
+                for (int i = 0; i < sequence.length; i++) {
+                    if (elem == sequence[i]) {
+                        return SearchResult.builder().withFound(true).withPosition(i).build();
+                    }
+                }
+                return SearchResult.builder().withFound(false).withPosition(-1).build();
+            }
+        };
+
+        similarityFinder = new SimilarityFinder(sequenceSearcher);
+        assertEquals(similarityFinder.calculateJackardSimilarity(seq5, seq4), 0.5);
+
+    }
+
+    //////////////////////////
+    @Test
+    void calculateJackardSimilarityCountInvoke() throws NoSuchFieldException, IllegalAccessException {
+        mock = new SequenceSearcher() {
+            int counter = 0;
+
+            @Override
+            public SearchResult search(int elem, int[] sequence) {
+                counter++;
+                return SearchResult.builder().withFound(true).build();
+            }
+        };
+        similarityFinder = new SimilarityFinder(mock);
+        similarityFinder.calculateJackardSimilarity(seq1, seq2);
+        Field field = mock.getClass().getDeclaredField("counter");
+        field.setAccessible(true);
+
+        res = field.getInt(mock);
+        assertEquals(res, 4);
+    }
+
+    @Test
+    void calculateJackardSimilarityCountZeroIfEmpty() throws NoSuchFieldException, IllegalAccessException {
+        mock = new SequenceSearcher() {
+            int counter = 0;
+
+            @Override
+            public SearchResult search(int elem, int[] sequence) {
+                counter++;
+                return SearchResult.builder().withFound(true).build();
+            }
+        };
+        similarityFinder = new SimilarityFinder(mock);
+        similarityFinder.calculateJackardSimilarity(seqEmpty, seqEmpty);
+        Field field = mock.getClass().getDeclaredField("counter");
+        field.setAccessible(true);
+
+        res = field.getInt(mock);
+        assertEquals(res, 0);
     }
 
 }
